c519f8240e6485c4165a1cbe7aadbc383129325f
hugangquan <hugangquan@ruigushop.com>
2021-04-10 19:53:14 +0800

    spring源码学习初次提交

---
+++ spring-study01/src/main/java/com/ruigu/spring/study/SpringStudyApplication.java
@@ -0,0 +1,17 @@
+package com.ruigu.spring.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hugangquan
+ * @date 2021/4/10 15:28
+ */
+@SpringBootApplication
+public class SpringStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStudyApplication.class,args);
+    }
+
+}
